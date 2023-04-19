1c0f9a0d9140c1e0bf90a171ea7dfce5e86ef524
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-04-19 09:35:19 -0700

    bonus done

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -21,6 +21,36 @@ export default class Calculator extends Component {
         })
     }
 
+    handleSubtract = () => {
+        let num1 = parseInt(this.state.num1)
+        let num2 = parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                sum: num1 - num2
+            }
+        })
+    }
+
+    handleMultiply = () => {
+        let num1 = parseInt(this.state.num1)
+        let num2 = parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                sum: num1 * num2
+            }
+        })
+    }
+
+    handleDivide = () => {
+        let num1 = parseInt(this.state.num1)
+        let num2 = parseInt(this.state.num2)
+        this.setState(() => {
+            return {
+                sum: num1 / num2
+            }
+        })
+    }
+
     render() {
         return(
             <div className="container">
@@ -33,7 +63,10 @@ export default class Calculator extends Component {
                     value={this.state.num1}
                     onChange={ (e) => this.setNum(e, 'num1') }
                 />
-                 <span>+</span>
+                 <button onClick={this.handleSum}>+</button>
+                 <button onClick={this.handleSubtract}>-</button>
+                 <button onClick={this.handleMultiply}>*</button>
+                 <button onClick={this.handleDivide}>/</button>
                 <input 
                     type="number"
                     name="num2"
@@ -41,9 +74,9 @@ export default class Calculator extends Component {
                     value={this.state.num2}
                     onChange={ (e) => this.setNum(e, 'num2') }
                 />
-                 <button onClick={this.handleSum}>=</button>         
-                    <h3>Addition results go here!</h3>
-                    <h3>{this.state.sum}</h3>
+                <span>=</span> 
+                <h3>Results go here!</h3>
+                <h3>{this.state.sum}</h3>
             </div>
         )
     }
